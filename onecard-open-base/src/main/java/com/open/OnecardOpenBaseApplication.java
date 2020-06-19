dd57acafeee1bf9af1ee43b0044114d50d935073
wuchuangpeng <wuchuanpeng@9fbank.com.cn>
2020-06-19 23:26:35 +0800

    Initial commit

---
+++ onecard-open-base/src/main/java/com/open/OnecardOpenBaseApplication.java
@@ -0,0 +1,13 @@
+package com.open;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnecardOpenBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnecardOpenBaseApplication.class, args);
+	}
+
+}
